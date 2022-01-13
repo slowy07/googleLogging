9576314ac29d2481404318a0cee4922c2373cdab
slowy07 <slowy.arfy@gmail.com>
2022-01-13 23:10:27 +0700

    test: reformatted code
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

--- src/cleanup_immediately_unittest.cc
+++ src/cleanup_immediately_unittest.cc
@@ -28,21 +28,22 @@
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <googlelog/logging.h>
+
 #include <googlelog/raw_logging.h>
 
 #include "base/commandlineflags.h"
+
 #include "googletest.h"
 
-#ifdef HAVE_LIB_GFLAGS
-#include <gflags/gflags.h>
+#ifdef HAVE_LIB_GFLAGS#include <gflags/gflags.h>
+
 using namespace GFLAGS_NAMESPACE;
 #endif
 
-#ifdef HAVE_LIB_GMOCK
-#include <gmock/gmock.h>
+#ifdef HAVE_LIB_GMOCK#include <gmock/gmock.h>
 
 #include "mock-log.h"
-// Introduce several symbols from gmock.
+ // Introduce several symbols from gmock.
 using GOOGLE_NAMESPACE::googlelog_testing::ScopedMockLog;
 using testing::_;
 using testing::AllOf;
@@ -66,12 +67,12 @@ TEST(CleanImmediately, logging) {
   google::DisableLogCleaner();
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char ** argv) {
   FLAGS_colorlogtostderr = false;
   FLAGS_timestamp_in_logfile_name = true;
-#ifdef HAVE_LIB_GFLAGS
-  ParseCommandLineFlags(&argc, &argv, true);
-#endif
+  #ifdef HAVE_LIB_GFLAGS
+  ParseCommandLineFlags( & argc, & argv, true);
+  #endif
   // Make sure stderr is not buffered as stderr seems to be buffered
   // on recent windows.
   setbuf(stderr, NULL);
@@ -86,10 +87,10 @@ int main(int argc, char **argv) {
 
   EXPECT_TRUE(IsGoogleLoggingInitialized());
 
-  InitGoogleTest(&argc, argv);
-#ifdef HAVE_LIB_GMOCK
-  InitGoogleMock(&argc, argv);
-#endif
+  InitGoogleTest( & argc, argv);
+  #ifdef HAVE_LIB_GMOCK
+  InitGoogleMock( & argc, argv);
+  #endif
 
   // so that death tests run before we use threads
   CHECK_EQ(RUN_ALL_TESTS(), 0);
